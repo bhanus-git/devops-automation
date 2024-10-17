bb2821168d077b2eb0fc80507a7fc3a0045b30d7
bhanus-git <bhanusbc@hotmail.com>
2024-10-17 18:43:39 +0530

    first commit

---
+++ src/main/java/com/example/devops_integration/DevopsIntegrationApplication.java
@@ -0,0 +1,26 @@
+package com.example.devops_integration;
+
+import jakarta.servlet.ServletOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devops")
+@SpringBootApplication
+public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public void getMessage()
+	{
+		System.out.println("welcome to devops-integration Application");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+
+}
